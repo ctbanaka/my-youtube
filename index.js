261d6f33dbd49cae269aa92d7eec55f3fbdb9280
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2024-04-12 18:12:24 +0530

    Add view engine and views directory configuration in index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+app.set('view engine', 'ejs');
 
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/watch', (req, res) => {
   const videoId = req.query.id;
